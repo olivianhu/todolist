134a7652d1a4a43ce0a53def5694ba5cd7a1848a
Olivia Hu <oliviahu2020@gmail.com>
2024-11-15 11:55:08 -0500

    Adds backend

--- src/App.js
+++ src/App.js
@@ -2,12 +2,39 @@ import './App.css';
 import {useState} from "react";
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import Axios from 'axios'
 
 function App() {
+  function retrieveTodos() {
+    Axios.get('http://localhost:5000/')
+    .then(function (response) {
+        console.log('response successfully received, response below')
+        console.log(response)
+
+        // set todos to be response we get from backend
+        setTodos(response.data);
+    }).catch(function (error) {
+        console.log('response unsusccessfully received, error below')
+        console.log(error)
+    })
+  }
+
+  function sendTodos(todos) {
+    Axios.post('http://localhost:5000/', todos)
+    .then(function (response) {
+        console.log('response successfully sent, response below')
+        console.log(response)
+    }).catch(function (error) {
+        console.log('response unsuccessfully received, error below')
+        console.log(error)
+    })
+  }
+
   const[todos, setTodos] = useState([]);
 
   function addTodo(todo) {
     setTodos([todo, ...todos]);
+    sendTodos(todo);
   }
 
   function removeTodo(id) {
@@ -33,7 +60,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>React Todo</p>
-        <TodoForm addTodo={addTodo}/>
+        <button
+          onClick={() => {retrieveTodos()}}
+        >Retrieve Past Todos</button>
+        <TodoForm 
+          addTodo={addTodo}/>
         <TodoList 
           todos={todos} 
           removeTodo={removeTodo}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,8 +3,9 @@ import Todo from "./Todo";
 export default function TodoList( {todos, removeTodo, toggleComplete} ) {
   return(
     <ul style={{ listStyle: 'none' }}>
-      {todos.map(todo => (
+      {todos.map((todo, index) => (
         <Todo 
+          key = {index}
           todo={todo} 
           removeTodo={removeTodo}
           toggleComplete={toggleComplete}
